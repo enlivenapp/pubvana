564f1330b34280de7736e0d3e61a4dd760f46b7d
Mike Webber <mw@enlivenapp.com>
2018-03-16 13:20:23 -0400

    theme options

--- pubvana/themes/default/theme_details.php
+++ pubvana/themes/default/theme_details.php
@@ -41,10 +41,56 @@
 		*/
 		public $version = '1.0.0';
 
+			/*
+			Enter the information in which you allow Widget areas,
+			and other options this theme has available.  We highly recommend using theme options 
+			for layout purposes as it allows users to customize their sites without editing HTML
+			code on their own.
+
+			Leave as blank array if there are no options. (see examples below)
+
+			Widgets: must be named 'widget_area_' then a number.  IE: widget_area_1, widget_area_2, etc.
+
+			Images: We attempt to find images (such as background images)  in two places. 
+					1: in /themes/[theme_name]/img/[image_name] IF there is only a file name. IE: background.jpg
+					2: use a complete URL (IE: http://example.com/images/someimage.jpg) 
+				Note: #2 allows use of the media manager to upload and use custom images
+
+				How to use: images are part of a multidimensional array.  simply declare the images in the 
+							'images' sub array area.  In your theme call the image thusly:
+							1: as img src:	<img src="<?= $this->template->[array_key] ?>" alt="<?php echo $template['title']; ?>">
+							2: or as a style element: <div style="background-image: url('<?= $this->template->[array_key] ?>');"> </div>
+
+							Example: $this->template->main_background  (see array example below)
+
+				Note: Use full words with underscores for your array keys as we humanize them in the form <label> and placeholder="" however, you are limited to 50 characters
+
+			EXAMPLE USES 
+
+				(with options):
+				public $options = 
+						[
+							// a simple array of widget areas you support in your theme
+							'widget_areas'	=> ['widget_area_1', 'widget_area_2'],
+
+							// an array of key/value pairs for images/videos/links you call in your theme
+							'images'		=> ['main_background' => 'background-2000x1333.jpg']
+							
+						];
+
+				(without options)
+				public $options = [
+						'widget_areas'	=> [],
+						'images'		=> []
+				];
+		*/
+		public $options = 
+		[
+			'widget_areas'	=> [],
+			'images'		=> []
+			
+		];
+
+
 
 }
-
-
-
-
-
