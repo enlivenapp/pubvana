02625660b95ecfe082b347e1d105053c7fd58635
Mike Webber <mw@enlivenapp.com>
2018-03-15 22:35:10 -0400

    of course...
    errors too…  they’re already in the .gitignore file

--- src/Installer.php
+++ src/Installer.php
@@ -105,8 +105,6 @@ class Installer
         unlink(__FILE__);
         //rmdir('application');
         unlink('composer.json.dist');
-        unlink('pubvana/config/config.php');
-        unlink('pubvana/config/database.php');
     }
 
     /**
